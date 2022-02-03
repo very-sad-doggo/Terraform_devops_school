97db9aa69db5a2e3301ecb2f1f43efb480f3d86b
этот василий <92812823+very-sad-doggo@users.noreply.github.com>
2022-02-03 22:30:39 +0300

    module adjustement

--- Task02/psql/main.tf
+++ Task02/psql/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_postgresql_server" "az_psql" {
-  name                = "az-${terraform.workspace}-psql"
+  name                = "task02-psql"
   location            = "${var.location}"
   resource_group_name = "${var.res_group_name}"
 
@@ -7,15 +7,11 @@ resource "azurerm_postgresql_server" "az_psql" {
   administrator_login_password = "${var.pgsql_password}"
 
   sku_name     = "B_Gen5_2"
-  #capacity = "${var.pgsql_capacity[terraform.workspace]}"
-  #tier     = "${var.pgsql_tier[terraform.workspace]}"
-  #family   = "Gen5"
 
-  storage_mb            = "${var.pgsql_storage[terraform.workspace]}"
-  backup_retention_days = "${var.pgsql_backup[terraform.workspace]}"
+  storage_mb            = "${var.pgsql_storage}"
+  backup_retention_days = "${var.pgsql_backup}"
   geo_redundant_backup_enabled = false
-  #geo_redundant_backup  = "${var.pgsql_redundant_backup[terraform.workspace]}"
-  
+    
   version                      = "9.6"
   ssl_enforcement_enabled              = true
  
