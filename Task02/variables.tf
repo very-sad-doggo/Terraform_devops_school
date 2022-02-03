3b3e2eaa957fe9cb964c623129b2e42330f604fb
этот василий <92812823+very-sad-doggo@users.noreply.github.com>
2022-02-03 22:27:05 +0300

    vars_fix

--- Task02/variables.tf
+++ Task02/variables.tf
@@ -35,47 +35,31 @@ variable "subnet_prefix" {
 #psql vars
 variable "pgsql_capacity" {
   description = "The scale up/out capacity, representing server's compute units."
-
-  default = {
-    prod = "30"
-    dev  = "2"
-  }
+  default = "2"
 }
 
 variable "pgsql_tier" {
   description = "Possible values are Basic, GeneralPurpose, and MemoryOptimized."
 
-  default = {
-    prod = "GeneralPurpose"
-    dev  = "Basic"
-  }
+  default = "Basic"
 }
 
 variable "pgsql_storage" {
   description = "Max storage allowed for a server Possible values are between 5120 MB(5GB) and 1048576 MB(1TB)"
 
-  default = {
-    prod = "10240"
-    dev  = "5120"
-  }
+  default = "5120"
 }
 
 variable "pgsql_backup" {
   description = "Backup retention days for the server, supported values are between 7 and 35 days."
 
-  default = {
-    prod = "35"
-    dev  = "7"
-  }
+  default = "7"
 }
 
 variable "pgsql_redundant_backup" {
   description = "Enable Geo-redundant or not for server backup"
 
-  default = {
-    prod = "Enabled"
-    dev  = "Disabled"
-  }
+  default = "Disabled"
 }
 
 variable "pgsql_password" {
