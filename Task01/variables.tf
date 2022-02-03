90b288925ae8513ab4d8aa25bc2e85dfb48152bf
этот василий <92812823+very-sad-doggo@users.noreply.github.com>
2022-02-03 19:50:29 +0300

    vars_fix

--- Task01/variables.tf
+++ Task01/variables.tf
@@ -1,3 +1,15 @@
+variable "subscription_id" {
+  description = "Azure subscription_id"
+}
+variable "client_id" {
+  description = "Azure client_id"
+}
+variable "client_secret" {
+  description = "Azure password"
+}
+variable "tenant_id" {
+  description = "Azure tenant_id"
+}
 variable "resource_group" {
     description = "resource group name"
     default = "vladimir-ryadovoy-diploma"
