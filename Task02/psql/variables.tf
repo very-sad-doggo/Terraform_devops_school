b750bb11e9a3b68a452da272a7381c26d92cdd54
этот василий <92812823+very-sad-doggo@users.noreply.github.com>
2022-02-03 22:29:14 +0300

    vars fix

--- Task02/psql/variables.tf
+++ Task02/psql/variables.tf
@@ -7,27 +7,22 @@ variable "res_group_name" {
 }
 
 variable "pgsql_capacity" {
-  type        = map(string)
-  description = "Azure resource group name"
+  description = "PSQL Capacity"
 }
 
 variable "pgsql_tier" {
-  type        = map(string)
   description = "The tier of the particular SKU"
 }
 
 variable "pgsql_storage" {
-  type        = map(string)
   description = "Max storage allowed for a server"
 }
 
 variable "pgsql_backup" {
-  type        = map(string)
   description = "Backup retention days for the server, supported values are between 7 and 35 days."
 }
 
 variable "pgsql_redundant_backup" {
-  type        = map(string)
   description = "Enable Geo-redundant or not for server backup"
 }
 
