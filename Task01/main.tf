edf92f81054d9e2c81a2aa172031e686a2960d52
этот василий <92812823+very-sad-doggo@users.noreply.github.com>
2022-02-03 19:47:50 +0300

    provider fix

--- Task01/main.tf
+++ Task01/main.tf
@@ -1,3 +1,10 @@
+rovider "azurerm" {
+  features {}
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
+} 
 data "azurerm_virtual_network" "vnet" {
     resource_group_name = "${var.resource_group}"
     name = "${var.vnet}"
